531d8d9a26f2a1f2eadd4e7ae5e53854ba21d37f
Josh <64652542+Tamely@users.noreply.github.com>
2023-04-20 19:23:32 -0500

    Init Repo

---
+++ wwwroot/js/script.js
@@ -0,0 +1,25 @@
+﻿window.weather = {
+    notFound: function() {
+        let container = document.querySelector('.container');
+        let weatherBox = document.querySelector('.weather-box')
+        let weatherDetails = document.querySelector('.weather-details')
+        let error404 = document.querySelector('.not-found')
+        
+        container.style.height = '400px';
+        weatherBox.style.display = 'none';
+        weatherDetails.style.display = 'none';
+        error404.style.display = 'block';
+        error404.classList.add('fadeIn');
+    },
+    show: function() {
+        let container = document.querySelector('.container');
+        let weatherBox = document.querySelector('.weather-box')
+        let weatherDetails = document.querySelector('.weather-details')
+        
+        weatherBox.style.display = '';
+        weatherDetails.style.display = '';
+        weatherBox.classList.add('fadeIn');
+        weatherDetails.classList.add('fadeIn');
+        container.style.height = '590px';
+    }
+}
